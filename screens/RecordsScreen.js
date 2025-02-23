1c742c215d70b8715a2c59361a9da448cc90f893
Alvila25 <florentmandoyves43@gmail.com>
2025-02-23 08:28:44 +0300

    Update RecordsScreen.js

--- screens/RecordsScreen.js
+++ screens/RecordsScreen.js
@@ -16,7 +16,7 @@ export default function RecordsScreen() {
       headers: { Authorization: `Bearer ${token}`, 'Content-Type': 'multipart/form-data' },
     });
     setHistory('');
-    setGreenhouse('');
+    settTelehealth('');
   };
 
   return (
